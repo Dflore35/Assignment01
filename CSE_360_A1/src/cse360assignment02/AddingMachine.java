306402fbba1066bb29bf665a63436fd7f5fd528a
Diana Flores <Dflore35@asu.edu>
2020-10-04 21:40:22 -0700

    Checking Assignment01
    Signed-off-by: Diana Flores<Dflore35@asu.edu>

---
+++ CSE_360_A1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,45 @@
+// Assignment #1: Arizona State University CSE360
+//         Name: Diana Flores
+//    StudentID: 1214750485
+//      Lecture: T 9 am
+//  Description: Assignment one helps us get familar with git hub.
+
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	
+	private int total; //declares the varible
+
+	public AddingMachine () 
+	{
+		total = 0;  // initializing total to zero
+	}
+
+	public int getTotal ()
+	{
+		return 0; //returns zero
+	}
+
+	//this method adds a value to total 
+	public void add (int value) 
+	{
+	}
+    //this method subtracts from the total
+	public void subtract (int value) 
+	{
+	}
+
+	//this method returns a string 
+	public String toString () 
+	{
+		return "";
+	}
+    //this method erases the memory
+	public void clear() 
+	{
+		
+	}
+
+
+}
